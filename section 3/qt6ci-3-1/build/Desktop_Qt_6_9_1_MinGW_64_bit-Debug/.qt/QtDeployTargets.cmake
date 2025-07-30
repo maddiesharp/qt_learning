3d5511e1bbb32651a66127dc4c5e6e489736e592
Madeline Sharp <maddiesharp95@gmail.com>
2025-07-30 12:48:35 -0400

    Added Udemy Code Examples

---
+++ section 3/qt6ci-3-1/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_qt6ci-3-1_FILE C:/Users/sharpm/repos/qt_learning/section 3/qt6ci-3-1/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/qt6ci-3-1.exe)
+set(__QT_DEPLOY_TARGET_qt6ci-3-1_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_qt6ci-3-1_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
